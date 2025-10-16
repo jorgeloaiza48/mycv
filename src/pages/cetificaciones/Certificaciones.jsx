8d18bd02b008ffd10e60d4995e4ac7e9fcf4b16d
jorgeloaiza48 <jorge.loaiza@utp.edu.co>
2025-10-16 14:41:02 -0500

    agregando certificaciones

--- src/pages/cetificaciones/Certificaciones.jsx
+++ src/pages/cetificaciones/Certificaciones.jsx
@@ -5,9 +5,11 @@ export default function Certificaciones() {
         <div>
             <hr className='custom'></hr>
             <h2 className='mt-5 text-center'>Certificaciones</h2>
-            <div className='cvMain'>
-                <img src="./iconos/oracleCertification.png" className="mx-auto d-block img-fluid" alt='' />
-            </div>
+            <div><img src="./iconos/oracleCertificationDBA.png"      className="mx-auto d-block img-fluid" alt='' /></div>
+            <div><img src="./iconos/ecertificateOCI-Foundations.png" className="mx-auto d-block img-fluid" alt='' /></div>
+            <div><img src="./iconos/CertifiedArchitectAssociate.png" className="mx-auto d-block img-fluid" alt='' /></div>
+            <div><img src="./iconos/OracleDataPlatform2025.png"      className="mx-auto d-block img-fluid" alt='' /></div>
+            <div><img src="./iconos/AIFoundationsAssociate.png"      className="mx-auto d-block img-fluid" alt='' /></div>
 
         </div>
     )
